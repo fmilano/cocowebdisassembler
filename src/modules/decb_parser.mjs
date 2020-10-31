40fcb3808e17e362395c78b1d6bfe7810a72831f
Federico Milano <fmilano@gmail.com>
2020-10-31 23:54:18 +0100

    Completed DECB parser. Added hex viewer.

--- src/modules/decb_parser.mjs
+++ src/modules/decb_parser.mjs
@@ -1,172 +1,128 @@
 export function decbParse(binaryBuffer) {
-  const states = {
-    PREAMBLE_MAGIC_NUMBER: "preamble_magic_number",
-    PREAMBLE_LEN_1: "preamble_len_1",
-    PREAMBLE_LEN_2: "preamble_len_2",
-    PREAMBLE_ADDR_1: "preamble_addr_1",
-    PREAMBLE_ADDR_2: "preamble_addr_2",
-    DATA: "data",
-    preamble_or_postamble_magic_number: "preamble_or_postamble_magic_number",
-    POSTAMBLE_ZERO_1: "postamble_zero_1",
-    POSTAMBLE_ZERO_2: "postamble_zero_2",
-    POSTAMBLE_EXEC_ADDR_1: "postamble_exec_addr_1",
-    POSTAMBLE_EXEC_ADDR_2: "postamble_exec_addr_2",
-    END: "end"
-  };
+    const states = {
+        PREAMBLE_MAGIC_NUMBER: "preamble_magic_number",
+        PREAMBLE_LEN_1: "preamble_len_1",
+        PREAMBLE_LEN_2: "preamble_len_2",
+        PREAMBLE_ADDR_1: "preamble_addr_1",
+        PREAMBLE_ADDR_2: "preamble_addr_2",
+        DATA: "data",
+        preamble_or_postamble_magic_number: "preamble_or_postamble_magic_number",
+        POSTAMBLE_ZERO_1: "postamble_zero_1",
+        POSTAMBLE_ZERO_2: "postamble_zero_2",
+        POSTAMBLE_EXEC_ADDR_1: "postamble_exec_addr_1",
+        POSTAMBLE_EXEC_ADDR_2: "postamble_exec_addr_2",
+        END: "end"
+    };
 
-  const inputBuffer = new Uint8Array(binaryBuffer);
+    const input = new Uint8Array(binaryBuffer);
 
-  let sections = [];
+    let sections = [];
 
-  let state = state.PREAMBLE_MAGIC_NUMBER;
+    let state = states.PREAMBLE_MAGIC_NUMBER;
 
-  let sectionLen = 0x00;
-  let beginAddr = 0x00;
-    int endAddr;
-    int execAddr;
+    let sectionLen = 0x00;
+    let beginAddr = 0x00;
+    let execAddr = 0x00;
 
-    size_t dataCount;
+    let dataCount = 0x00;
 
-    for (let i = 0; i < inputBuffer.byteLength(); ++i)
-    {
-        if (State::PREAMBLE_MAGIC_NUMBER == state)
-        {
-            if (input[i] != 0)
-            {
-                std::cerr << "Invalid first preamble magic number. Parsing aborted." << std::endl;
-                return 1;
+    let initDataSectionIndex;
+
+    for (let i = 0; i < input.byteLength; ++i) {
+
+        if (states.PREAMBLE_MAGIC_NUMBER === state) {
+            if (input[i] != 0) {
+                return { status: -1, msg: "Invalid first preamble magic number. Parsing aborted." };
             }
 
-            state = State::PREAMBLE_LEN_1;
+            state = states.PREAMBLE_LEN_1;
 
         }
-        else if (State::PREAMBLE_LEN_1 == state)
-        {
+        else if (states.PREAMBLE_LEN_1 === state) {
             sectionLen = input[i];
             sectionLen <<= 8;
 
-            state = State::PREAMBLE_LEN_2;
+            state = states.PREAMBLE_LEN_2;
         }
-        else if (State::PREAMBLE_LEN_2 == state)
-        {
+        else if (states.PREAMBLE_LEN_2 === state) {
             sectionLen += input[i];
 
-            state = State::PREAMBLE_ADDR_1; 
+            state = states.PREAMBLE_ADDR_1;
         }
-        else if (State::PREAMBLE_ADDR_1 == state)
-        {
+        else if (states.PREAMBLE_ADDR_1 === state) {
             beginAddr = input[i];
             beginAddr <<= 8;
 
-            state = State::PREAMBLE_ADDR_2;
+            state = states.PREAMBLE_ADDR_2;
         }
-        else if (State::PREAMBLE_ADDR_2 == state)
-        {
+        else if (states.PREAMBLE_ADDR_2 === state) {
             beginAddr += input[i];
 
             dataCount = 0;
-            state = State::DATA;
-
-            ++parsedSections;
-
-            std::cout << "Section: " << parsedSections << std::endl;
-            std::cout << "Address: 0x" << std::hex << beginAddr << std::dec << " (" << beginAddr << ")" << std::endl;
-            std::cout << "Length: " << sectionLen << " bytes" << std::endl;
-            std::cout << "------------------------" << std::endl;
-
+            initDataSectionIndex = i + 1;
+            state = states.DATA;
         }
-        else if (State::DATA == state)
-        {
-            if (0 == dataCount)
-            {
-                size_t lastindex = inputFile.find_last_of(".");
-
-                std::string rawname = inputFile;
-                if (lastindex != std::string::npos)
-                    rawname = inputFile.substr(0, lastindex);
-
-                std::stringstream ss; 
-                ss << rawname << "_" << std::hex << std::setfill('0') << std::setw(4) << beginAddr << ".bin";
-
-                ofile.open(ss.str(), std::ios::binary);
-                if (!ofile.is_open())
-                {
-                    std::cerr << "Could not open output file " << ss.str() << ". Parsing aborted." << std::endl;
-                    return 1;
-                }
-            }
-
-            ofile.put(input[i]);
+        else if (states.DATA === state) {
             ++dataCount;
 
-            if (dataCount >= sectionLen)
-            {
-                ofile.close();
-                state = State::PREAMBLE_OR_POSTAMBLE_MAGIC_NUMBER;
-            }
-        }
-        else if (State::PREAMBLE_OR_POSTAMBLE_MAGIC_NUMBER == state)
-        {
-            if (0 == input[i])
-            {
-                state = State::PREAMBLE_LEN_1;
-            }
-            else if (0xFF == input[i])
-            {
-                state = State::POSTAMBLE_ZERO_1;
-            }
-            else
-            {
-                std::cerr << "Preamble/postamble magic number not found. Parsing aborted." << std::endl;
-                return 1;
-            }            
-        }
-        else if (State::POSTAMBLE_ZERO_1 == state)
-        {
-            if (input[i] != 0x00)
-                std::cout << "Warning! Postable zero 1 is not 0." << std::endl;
+            if (dataCount >= sectionLen) {
 
-            state = State::POSTAMBLE_ZERO_2;
+                sections.push({ startAddress: beginAddr, length: dataCount, data: input.slice(initDataSectionIndex, i+1) });
+                state = states.PREAMBLE_OR_POSTAMBLE_MAGIC_NUMBER;
+            }
         }
-        else if (State::POSTAMBLE_ZERO_2 == state)
-        {
-            if (input[i] != 0x00)
-                std::cout << "Warning! Postable zero 2 is not 0." << std::endl;
-                
-            state = State::POSTAMBLE_EXEC_ADDR_1;
+        else if (states.PREAMBLE_OR_POSTAMBLE_MAGIC_NUMBER === state) {
+            if (0 === input[i]) {
+                state = states.PREAMBLE_LEN_1;
+            }
+            else if (0xFF === input[i]) {
+                state = states.POSTAMBLE_ZERO_1;
+            }
+            else {
+                return { status: -1, msg: "Preamble/postamble magic number not found. Parsing aborted." };
+            }
         }
-        else if (State::POSTAMBLE_EXEC_ADDR_1 == state)
-        {
+        else if (states.POSTAMBLE_ZERO_1 === state) {
+            //if (input[i] != 0x00)
+            //    std::cout << "Warning! Postable zero 1 is not 0." << std::endl;
+
+            state = states.POSTAMBLE_ZERO_2;
+        }
+        else if (states.POSTAMBLE_ZERO_2 === state) {
+            //if (input[i] != 0x00)
+            //    std::cout << "Warning! Postable zero 2 is not 0." << std::endl;
+
+            state = states.POSTAMBLE_EXEC_ADDR_1;
+        }
+        else if (states.POSTAMBLE_EXEC_ADDR_1 === state) {
             execAddr = input[i];
             execAddr <<= 8;
 
-            state = State::POSTAMBLE_EXEC_ADDR_2;
+            state = states.POSTAMBLE_EXEC_ADDR_2;
 
         }
-        else if (State::POSTAMBLE_EXEC_ADDR_2 == state)
-        {
+        else if (states.POSTAMBLE_EXEC_ADDR_2 === state) {
             execAddr += input[i];
-            state = State::END;
+            state = states.END;
         }
-        else
-        {
-            std::cerr << "Invalid parser state." << std::endl;
-            return 1;
+        else {
+            return { status: -1, msg: "Invalid parser state." };
         }
-        
+
     }
 
-    if (state != State::END)
-    {
-        std::cerr << "Invalid input file." << std::endl;
-        return 1;
+    if (state != states.END) {
+        return { status: -1, msg: "Invalid input." };
     }
 
-    std::cout << "Execution address: " << std::hex << execAddr << std::dec << " (" << execAddr << ")" << std::endl;
-            
-    std::cout << "Total sections: " << parsedSections << std::endl;
 
-  // object literal notation to create your structures
-  sections.push({ startAddress: 'abc', dissamblyString: 'LDA $F991', instruction: '', comment: '' });
-}; 
+    return { status: 0, execAddress: execAddr, sections: sections };
+
+    //std::cout << "Execution address: " << std::hex << execAddr << std::dec << " (" << execAddr << ")" << std::endl;
+
+    //std::cout << "Total sections: " << parsedSections << std::endl;
+
+    // object literal notation to create your structures
+
+};
 
---
+++ src/view/hex0r.js
@@ -0,0 +1,169 @@
+var BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+var HEX = '0123456789ABCDEF';
+
+function markup_hex0rwindow(div) {
+    var step = parseInt($(div).data('row-width'));
+    var wordSize = parseInt($(div).data('word-size'));
+    var rowBreak = parseInt($(div).data('row-break'));
+    var caption = $(div).data('caption');
+    var highlightsStr = $(div).data('highlights').split(',');
+    var trim = $(div).data('trim').toString() == "true"; ;
+    var base64 = $(div).data('base64').toString() == "true";
+    var showLineNums = $(div).data('show-line-nums').toString() == "true";
+    var rawData = div.text();
+
+    if (trim == true) {
+        rawData = remove_whitespace(rawData);
+    }
+
+    if (base64 == true) {
+        rawData = base64_decode(rawData);
+    }
+    var lineData;
+
+    var highlights = [];
+
+    for (var hi = 0; hi < highlightsStr.length; hi++) {
+        highlights.push(highlightsStr[hi].split(":"));
+    }
+
+    div.text("");
+    div.append("<table></table>");
+
+    var offset = 0;
+
+    function applyHighlights(index) {
+        for (var idx = 0; idx < highlights.length; idx++) {
+            if ((index >= highlights[idx][0]) && (index <= highlights[idx][1])) {
+                if (index == highlights[idx][0]) {
+                    $("table tr td:last", div).addClass("hex0rwindow_border_start");
+                }
+
+                if (index == highlights[idx][1]) {
+                    $("table tr td:last", div).addClass("hex0rwindow_border_end");
+                }
+
+                $("table tr td:last", div).addClass("hex0rwindow_code_hi hex0rwindow_border_middle");
+                $("table tr td:last", div).attr("style", "backround-color: " + highlights[idx][2] + ";");
+                $("table tr td:last", div).attr("title", highlights[idx][3]);
+
+                runlen += 1;
+            }
+            else {
+                $("table tr td:last", div).addClass("hex0rwindow_code");
+            }
+        }
+    }
+
+    if (caption)
+        $("table", div).append("<caption>" + caption + "</caption>");
+
+    while (rawData.length > 0) {
+        lineData = rawData.slice(0, step);
+        rawData = rawData.slice(step);
+
+        $("table", div).addClass("hex0rwindow_table");
+        $("table", div).append("<tr></tr>").addClass("hex0rwindow");
+
+        if (showLineNums == true) {
+            $("table tr:last", div).append("<td>" + dec_to_hex8(offset) + " </td>");
+            $("table tr td:last", div).addClass("hex0rwindow_offset");
+        }
+        var runlen = 0;
+
+        for (var idxData = 0; idxData < lineData.length; idxData += wordSize) {
+            var num = "";
+
+            for (var idxWs = 0; idxWs < wordSize; idxWs++) {
+                num += dec2_to_hex(lineData.charCodeAt(idxData + idxWs));
+            }
+            if (idxData == rowBreak - 1) {
+                $("table tr:last", div).append("<td>" + num + "&nbsp;&nbsp;&nbsp</td>");
+
+                applyHighlights(offset + idxData);
+
+            }
+            else {
+                $("table tr:last", div).append("<td>" + num + " </td>");
+
+                applyHighlights(offset + idxData);
+            }
+        }
+
+        var text = "";
+
+        for (var i = 0; i < lineData.length; i++) {
+            var cc = lineData.charCodeAt(i);
+
+            if ((cc >= 32) && (cc <= 126)) {
+                text = text + lineData.charAt(i);
+            }
+            else {
+                text = text + ".";
+            }
+        }
+
+        if (lineData.length < step)
+            $("table tr td:last", div).attr("colspan", Math.floor((step - lineData.length) / wordSize) + 1);
+
+        offset += step;
+
+        $("table tr:last", div).append("<td>" + text + "</td>");
+        $("table tr td:last", div).addClass("hex0rwindow_visual");
+    }
+}
+
+function dec2_to_hex(dec) {
+    if (dec < 0)
+        dec = 0;
+
+    if (dec > 255)
+        dec = 255;
+
+    return HEX.charAt(Math.floor(dec / 16)) + HEX.charAt(dec % 16);
+}
+
+function dec_to_hex8(dec) {
+    var str = "";
+
+    for (var i = 3; i >= 0; i--) {
+        str += dec2_to_hex((dec >> (i * 8)) & 255);
+    }
+
+    return str;
+}
+
+function remove_whitespace(str) {
+    return str.replace(/\n/g, "")
+					  .replace(/\t/g, "")
+					  .replace(/ /g, "")
+					  .replace(/\r/g, "");
+}
+
+function base64_decode(encoded) {
+    var decoded = "";
+
+    for (var i = 0; i < encoded.length; i += 4) {
+        var ch0 = encoded.charAt(i + 0);
+        var ch1 = encoded.charAt(i + 1);
+        var ch2 = encoded.charAt(i + 2);
+        var ch3 = encoded.charAt(i + 3);
+
+        var index0 = BASE64_CHARS.indexOf(ch0);
+        var index1 = BASE64_CHARS.indexOf(ch1);
+        var index2 = BASE64_CHARS.indexOf(ch2);
+        var index3 = BASE64_CHARS.indexOf(ch3);
+
+        decoded += String.fromCharCode((index0 << 2) | (index1 >> 4));
+        decoded += String.fromCharCode(((index1 & 15) << 4) | (index2 >> 2));
+        decoded += String.fromCharCode(((index2 & 3) << 6) | index3);
+    }
+
+    return decoded;
+}
+
+$(document).ready(function () {
+    $("div.hex0rwindow").each(function (index) {
+        markup_hex0rwindow($(this), index);
+    });
+});
