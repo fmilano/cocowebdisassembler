ced8681a20d3f1f6b759978ffd7b4992e046b211
Federico Milano <fmilano@gmail.com>
2020-11-21 22:35:55 +0100

    Updates the NavBar while changing pages.

--- src/view/setupNavBar.mjs
+++ src/view/setupNavBar.mjs
@@ -1,15 +1,39 @@
 
 export function setupNavBar(binaryBuffer, offset) {
+
+    let currentLocation = window.location.pathname;
+
+
+    let uploadActive = "";
+    let sectionsActive = "";
+    let disassemblyActive = "";
+    let hexActive = "";
+
+    switch(currentLocation) {
+        case "/sections.html":
+            sectionsActive = " active";
+            break;
+        case "/index.html":
+            uploadActive = " active";
+            break;
+        case "/disassembly.html":
+            disassemblyActive = " active";
+            break;
+        case "/hex.html":
+            hexActive = " active";
+            break;
+        default:
+            uploadActive = " active";    
+    }
+
     document.getElementById('navbar').innerHTML = 
     `<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="index.html">Upload <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="listSections.html">Sections</a>
-                <a class="nav-item nav-link" href="#">Disassembly</a>
-                <a class="nav-item nav-link disabled" href="#">Hex</a>
+                <a class="nav-item nav-link` + uploadActive + `" href="index.html">Upload</a>
+                <a class="nav-item nav-link` + sectionsActive + `" href="sections.html">Sections</a>
+                <a class="nav-item nav-link` + disassemblyActive + `" href="disassembly.html">Disassembly</a>
+                <a class="nav-item nav-link` + hexActive + `" href="hex.html">Hex</a>
             </div>
-        </div>
     </nav>`;
 
 
